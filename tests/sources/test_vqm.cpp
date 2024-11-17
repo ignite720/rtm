17805d8af16da2d4f324e5995e9fd50843637103
Nicholas Frechette <zeno490@gmail.com>
2024-11-17 10:29:27 -0500

    docs(tests): update copyright date

--- tests/sources/test_vqm.cpp
+++ tests/sources/test_vqm.cpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // The MIT License (MIT)
 //
-// Copyright (c) 2023 Nicholas Frechette & Realtime Math contributors
+// Copyright (c) 2024 Nicholas Frechette & Realtime Math contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
